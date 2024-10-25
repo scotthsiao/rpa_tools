77cc69c753d0d73b0b01190850ac88ed1a72bf53
Scott <yuying@webglsoft.com>
2024-10-26 06:03:24 +0800

    feat: add njav.tv parser and simple requests loader

--- sources/missav_to_ob/scrape_missav_to_note.py
+++ sources/missav_to_ob/scrape_missav_to_note.py
@@ -1,3 +1,4 @@
+from typing import List
 import os.path
 import time
 
@@ -6,13 +7,14 @@ from requests_html import HTML
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
+from socks import method
 from webdriver_manager.chrome import ChromeDriverManager
 from lxml import etree
 
-test_url='https://missav.com/dvdes-543-uncensored-leak'
+test_url='https://njav.tv/zh/dm1/v/dvaj-615'
 ob_vaults_path = r"C:\Users\Scott\OB\卡片庫\AV Collections"
 
-class MissavInfo:
+class AvTitleInfo:
     def __init__(self):
         self.title: str = None
         self.thumbnail: str = None
@@ -22,7 +24,7 @@ class MissavInfo:
         self.director: str = None
         self.release_date: str = None
         self.source_link: str = None
-        self.tags = []
+        self.tags: List[str] = []
 
     def __str__(self):
         uncensored = True if 'uncensored-leak' in self.source_link else False
@@ -50,8 +52,8 @@ Stars:
 ---"""
         return serialized_data
 
-    def write_to_file(self, filepath) -> bool:
-        print(self.__str__())
+    def write_to_file(self, filepath:str) -> bool:
+        print(filepath)
         with open(filepath, 'w+', encoding='utf-8') as fd:
             fd.write(self.__str__())
 
@@ -62,27 +64,57 @@ def request_page_source(url:str) -> str:
     # Check if the request was successful
     if response.status_code != 200:
         raise ValueError(f"Request failed with status code: {response.status_code}")
-    html = HTML(html=response.text)
+    # html = HTML(html=response.text)
+    return response.text
 
 
+def get_page_source(url: str, method:str = 'selenium') -> str:
+    page_source = None
+    if method == 'selenium':
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-extensions')
 
-def get_page_source(url: str) -> str:
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-extensions')
+        options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36")
+        dr = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        dr.get(url)
+        page_source = dr.page_source
+    elif method == 'requests':
+         page_source = request_page_source(url)
+    return page_source
 
-    options.add_argument(
-        "user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36")
-    dr = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    dr.get(url)
-    return dr.page_source
+def parse_njav_info(raw: str) ->AvTitleInfo:
+    info = AvTitleInfo()
+    html  = HTML(html=raw)
+
+    info.title = html.find('h1', first=True).text
+    data_poster_div = html.find('div[data-poster]', first=True)
+    if data_poster_div:
+        info.thumbnail = data_poster_div.attrs.get('data-poster')
 
 
-def parse_missav_info(raw: str) -> MissavInfo:
-    info = MissavInfo()
+    content_div = html.find('div.content', first=True)
+    if content_div:
+        # Extract and print the text or HTML from the 'content' div
+        # print(content_div.text)  # or use content_div.html for raw HTML
+        info.release_date = html.find('span', containing="发布日期:", first=True).element.getnext().text
+        info.serial =  html.find('span', containing="代码:", first=True).element.getnext().text
+        company_ele = html.find('span', containing="制作者:", first=True).element.getnext()
+        info.company = company_ele.find('a').text
+        actress_ele = html.find('span', containing="演员:", first=True).element.getnext()
+        actress = actress_ele.text_content().strip().split('\n')
+        info.artists = ', '.join(actress)
+    else:
+        print("Content div not found.")
+    return info
+
+
+def parse_missav_info(raw: str) -> AvTitleInfo:
+    info = AvTitleInfo()
 
     parser = etree.HTMLParser()
     tree = etree.fromstring(raw, parser)
@@ -113,18 +145,25 @@ def parse_missav_info(raw: str) -> MissavInfo:
 
 
 if __name__ == "__main__":
+    # use parser to get url option from CLI
+
     time_start = time.time()
-    raw_html = get_page_source(test_url)
-    print(f"{len(raw_html)=} delta {time.time() - time_start}")
-    info = parse_missav_info(raw_html)
+    if test_url.startswith('https://missav.com'):
+        raw_html = get_page_source(test_url, 'selenium')
+        print(f"{len(raw_html)=} delta {time.time() - time_start}")
+        info = parse_missav_info(raw_html)
+    elif test_url.startswith('https://njav.tv'):
+        raw_html = get_page_source(test_url, 'requests')
+        print(f"{len(raw_html)=} delta {time.time() - time_start}")
+        info = parse_njav_info( raw_html)
     info.source_link = test_url
-    info.write_to_file(os.path.join(ob_vaults_path, info.title + '.md'))
+
+    # replace  '/' with '-' in title
+    filepath = info.title.replace('/', '-')
+    info.write_to_file(os.path.join(ob_vaults_path, filepath + '.md'))
 
 
-# $x(".//H1")
-# $x('//time')
-# $x("//div[contains(@x-show,'detail')]")
-# $x("//span[contains(text(),'番號')]/../span[2]/text()")
-# $x("//span[contains(text(),'女優')]/../a/text()")
-# $x("//span[contains(text(),'發行商')]/../a/text()")
-# $x("//span[contains(text(),'導演')]/../a/text()")
+
+
+
+
